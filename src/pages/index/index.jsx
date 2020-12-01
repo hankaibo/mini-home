e5681c9851f75c8c6ec6ae7096eaea238833d468
hankaibo <hankaibo@gsafety.com>
2020-12-01 15:24:50 +0800

    feat: 添加示例

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Swiper, SwiperItem, Image } from '@tarojs/components'
 import './index.less'
 
 export default class Index extends Component {
@@ -8,21 +8,56 @@ export default class Index extends Component {
     navigationBarTitleText: '首页'
   }
 
-  componentWillMount () { }
+  componentWillMount() {
+  }
 
-  componentDidMount () { }
+  componentDidMount() {
+  }
 
-  componentWillUnmount () { }
+  componentWillUnmount() {
+  }
 
-  componentDidShow () { }
+  componentDidShow() {
+  }
 
-  componentDidHide () { }
+  componentDidHide() {
+  }
 
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-      </View>
+      <Swiper
+        indicatorDots
+        indicatorColor='#999'
+        indicatorActiveColor='#333'
+        autoplay
+        circular
+        className='test-h'
+      >
+        <SwiperItem>
+          <Image
+            style='background: #fff;'
+            src='https://img.soufunimg.com/news/2015_05/13/zhishi/1431510486917_000.jpg'
+          />
+        </SwiperItem>
+        <SwiperItem>
+          <Image
+            style='background: #fff;'
+            src='https://img.soufunimg.com/news/2015_05/13/zhishi/1431510069190_000.jpg'
+          />
+        </SwiperItem>
+        <SwiperItem>
+          <Image
+            style='background: #fff;'
+            src='https://img.soufunimg.com/news/2015_05/13/zhishi/1431510067943_000.jpg'
+          />
+        </SwiperItem>
+        <SwiperItem>
+          <Image
+            style='background: #fff;'
+            src='https://img.soufunimg.com/news/2015_05/13/zhishi/1431510067943_000.jpg'
+          />
+        </SwiperItem>
+      </Swiper>
     )
   }
 }
